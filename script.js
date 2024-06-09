bd83ccced5a56ca19d13bd1733e5a6b1413ef8ee
MPerera <73799288+mper-era@users.noreply.github.com>
2024-06-09 13:29:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 window.onload = function(){
 
+    // Changing variables
+    var pg = 8016; // Current page
+    var chap = 0; // Current chapter
+    var date = new Date("6/9/2024 6:19 AM UTC"); // Current time/date
+
     var pb = document.getElementById("progbar");
 
-    // Changing variables
-    var cur = 8016;
-    var date = new Date("6/9/2024 6:19:20 AM UTC");
-    date.toString();
+    var minpg = 0;
+    var maxpg = 31523;
+    pb.value = pg;
+    pb.max = maxpg;
 
-    var min = 0;
-    var max = pb.max;
-    pb.value = cur;
+    document.getElementById("minnum").innerHTML = minpg.toString();
+    document.getElementById("curnum").innerHTML = pg.toString();
+    document.getElementById("maxnum").innerHTML = maxpg.toString();
 
-    document.getElementById("minnum").innerHTML = min.toString();
-    document.getElementById("curnum").innerHTML = cur.toString();
-    document.getElementById("maxnum").innerHTML = max.toString();
-
-    var percent = 3 + ((88 - 5) * (cur / max))
+    var percent = 3 + ((88 - 5) * (pg / maxpg))
     document.getElementById("curnum").style.transform = `translate(${percent}%, -3.03488372093vw)` // -43.5px
 
     var dp = 1000
-    percent = Math.round((cur / max) * 100 * dp) / dp
+    percent = Math.round((pg / maxpg) * 100 * dp) / dp
     document.getElementById("percent").innerHTML = percent.toString().concat("%");
 
+    date = date.toString();
+    document.getElementById("datetxt").innerHTML = `Last Updated on ${date.substring(4, 21)}`;
 
+    
 }
