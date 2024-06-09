3202758e61b55823ba8947b070616dd01248dc26
MPerera <73799288+mper-era@users.noreply.github.com>
2024-06-09 01:33:30 -0500

    Scaling Feature (#1)
    * Update index.html
    
    * Update script.js
    
    * Update style.css

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ window.onload = function(){
 
     var pb = document.getElementById("progbar");
 
+    // Changing variables
+    var cur = 8016;
+    var date = new Date("6/9/2024 6:19:20 AM UTC");
+    date.toString();
+
     var min = 0;
-    var cur = pb.value;
     var max = pb.max;
+    pb.value = cur;
 
     document.getElementById("minnum").innerHTML = min.toString();
     document.getElementById("curnum").innerHTML = cur.toString();
     document.getElementById("maxnum").innerHTML = max.toString();
 
     var percent = 3 + ((88 - 5) * (cur / max))
+    document.getElementById("curnum").style.transform = `translate(${percent}%, -3.03488372093vw)` // -43.5px
 
-    document.getElementById("curnum").style.transform = `translate(${percent}%, -43.5px)`
+    var dp = 1000
+    percent = Math.round((cur / max) * 100 * dp) / dp
+    document.getElementById("percent").innerHTML = percent.toString().concat("%");
+
+
 }
