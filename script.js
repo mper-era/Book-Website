2bb374ea427a44f3b6ea0da941e00e7f66a6fbfe
MPerera <73799288+mper-era@users.noreply.github.com>
2024-06-13 07:49:10 +0530

    Update reading progress

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function() {
 
     // Changing variables
-    var pg = 8629; // Current page
-    var chap = 640; // Current chapter
-    var date = new Date("6/12/2024 06:31 UTC"); // Current time/date
+    var pg = 8852; // Current page
+    var chap = 655; // Current chapter
+    var date = new Date("6/13/2024 02:18 UTC"); // Current time/date
 
     var pb = document.getElementById("progbar");
 
